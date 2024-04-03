b904d0ce1cdd5d06bb3dfb892c51a463a9693a18
Salvador Azcon Garcia <saazcon-@c2r7s1.42madrid.com>
2024-04-03 19:55:05 +0200

    100%

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef Fixed_HPP
+# define Fixed_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	private:
+		int i;
+		static const int j = 8;
+
+	public:
+		Fixed();
+		Fixed( const Fixed &cp );
+		~Fixed();
+		Fixed& operator=( const Fixed &cp );
+
+		int getRawBits( void ) const;
+		void setRawBits ( int const raw );
+};
+
+#endif
