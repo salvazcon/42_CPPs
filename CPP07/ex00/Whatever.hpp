9f5a8387c0e5ef6b841317f581e6c779ee0402f0
salvador <salvazcon@gmail.com>
2024-07-12 12:31:03 +0200

    fixing sintaxis error

--- CPP07/ex00/Whatever.hpp
+++ CPP07/ex00/Whatever.hpp
@@ -5,19 +5,22 @@
 #include <string>
 
 template <typename T>
-void swap(T a, T b) {
+void swap(T a, T b)
+{
     T aux = a;
     a = b;
     b = aux;
 }
 
 template <typename T>
-T max(T a, T b) {
+T max(T a, T b) 
+{
     return (a > b) ? a : b;
 }
 
 template <typename T>
-T min(T a, T b) {
+T min(T a, T b) 
+{
     return (a < b) ? a : b;
 }
 
--- CPP07/ex01/Iter.hpp
+++ CPP07/ex01/Iter.hpp
@@ -4,6 +4,23 @@
 #include <iostream>
 #include <string>
 
+template <typename T>
+void swap(T a, T b) {
+    T aux = a;
+    a = b;
+    b = aux;
+}
+
+template <typename T>
+T max(T a, T b) {
+    return (a > b) ? a : b;
+}
+
+template <typename T>
+T min(T a, T b) {
+    return (a < b) ? a : b;
+}
+
 class Iter
 {
     protected:
